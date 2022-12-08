596edfc55de44ce912b4122d0c3bb298b9641c03
Alexis Lopez <alexislopez@outlook.com.ar>
2022-12-08 15:35:35 +0000

    [ADD] basic module files

---
+++ space_mission/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Space Mission',
+    'description': '''
+        Helps organice and manage Odoo's Moon travel. It manages:
+        - Space Crew
+        - Space Ship
+    ''',
+    'summary': 'Odoo Inc module to manage the travel to the Moon',
+    'author': 'Alexis Lopez',
+    'category': 'Traveling',
+    'version': '1.0.0',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+}
