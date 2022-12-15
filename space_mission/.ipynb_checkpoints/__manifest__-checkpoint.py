60c4df64463b60bc21f423ac7e7cdb2cf10df239
Alexis Lopez <alexislopez@outlook.com.ar>
2022-12-15 16:44:31 +0000

    [ADD] mission_info model, view and permitions

--- space_mission/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ space_mission/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -16,6 +16,8 @@
         'security/space_mission_security.xml',
         'security/ir.model.access.csv',
         'views/space_mission_menu.xml',
+        'views/space_ship_view.xml',
+        'views/mission_info.xml',
     ],
     'demo': [
         'demo/space_mission_demo.xml',
@@ -23,7 +25,4 @@
     'installable': True,
     'application': True,
     'auto_install': False,
-    'depends': ['base'],
-    'data': [],
-    'demo': [],
 }
